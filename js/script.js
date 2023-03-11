f5f860d8fae6a736bc0b96ee9a75478cb2d9112b
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-03-11 15:11:07 +0100

    funzioni random

--- js/script.js
+++ js/script.js
@@ -24,10 +24,49 @@ SubmitBtn.addEventListener("click", function() {
     let FinalPrice = PriceKm * UserKm
         if(document.getElementById("user-age").value==="Minorenne"){
             FinalPrice=FinalPrice-(FinalPrice*20/100)
+            offer = "Sconto 20% Under 18"
         }
         else if(document.getElementById("user-age").value==="Over65"){
             FinalPrice=FinalPrice-(FinalPrice*40/100)
+            offer = "Sconto 40% Over 65"
         }
+        else if(document.getElementById("user-age").value==="Maggiornne"){
+            FinalPrice=FinalPrice-(FinalPrice*40/100)
+            offer = "Nessun offerta"
+        }
+
+        document.getElementById("biglietto").innerHTML = offer
+
+        // Calcolo random da 1 a 8
+        const cabin = Math.floor(Math.random() * 8) + 1;
+        document.getElementById("carrozza").innerHTML = cabin
+
+        // Calcolo random da 1000 a 9999
+        const code = Math.floor(Math.random() * (99999 - 10000 + 1) ) + 10000;
+        document.getElementById("code-cp").innerHTML = code
         
-    Result.innerHTML = FinalPrice
+    Result.innerHTML = FinalPrice 
+
+        // Ripulire gli input
+        UserNameInput.value = "";
+        UserKmInput.value = "";
+        UserAgeInput.value = "";
+
+});
+
+// Implementazione della funzionalità annulla
+const clearBtn = document.getElementById("reset-btn");
+clearBtn.addEventListener("click", function () {
+  // Ripulire e nascondere il biglietto
+  passengerNameElem.innerHTML = "";
+  offerElem.innerHTML = "";
+  cabinElem.innerHTML = "";
+  codeElem.innerHTML = "";
+  priceElem.innerHTML = "";
+  document.getElementById("background-price").classList.add("hidden");
+
+  // Ripulire gli input
+  nameInput.value = "";
+  kmInput.value = "";
+  ageRangeInput.value = "";
 });
